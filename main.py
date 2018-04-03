83aba81ab02bea987d0cd73f8abc51add09ecf20
FARAZ TALEBI <S331419119@ocdsb>
2018-04-03 09:33:40 -0400

    part 2

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# Faraz
 # March 28, 2018
+             
+def CtoF (temp):
+    temp = round(32+(temp*1.8))
+    return temp
 
-###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def FtoC (temp):
+    temp = round((0.55556)*(temp-32))
+    return temp
 
-###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
-
-temperature = int(input('Enter your temperature in Celsius: '))
-temperature = 32+(temperature*1.8)
-print(round(temperature))
+choice = int(input("Enter 1 for C to F, Enter 2 for F to C:"))
+a = int(input("Enter the temperature you would like to convert:"))
+if choice == 1:
+        print(CtoF(a))
+else:
+        print(FtoC(a))
