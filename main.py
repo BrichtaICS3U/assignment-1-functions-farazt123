a21b68df18bb94718d4bde4afbe0916bf608ae57
FARAZ TALEBI <S331419119@ocdsb>
2018-04-04 10:01:29 -0400

    Finished assignment

--- main.py
+++ main.py
@@ -10,10 +10,17 @@ def CtoF (temp):
 def FtoC (temp):
     temp = round((0.55556)*(temp-32))
     return temp
-
-choice = int(input("Enter 1 for C to F, Enter 2 for F to C:"))
-a = int(input("Enter the temperature you would like to convert:"))
-if choice == 1:
+repeat = 1
+while repeat == 1:
+    choice = int(input("Enter 1 for C to F, Enter 2 for F to C:"))
+    a = int(input("Enter the temperature you would like to convert:"))
+    while ( choice == 1 and a <= -273 ) or ( choice == 2 and a <= -459 ):
+        print("The temperature you have entered is below absolute zero")
+        break
+    if choice == 1:
         print(CtoF(a))
-else:
+    else:
         print(FtoC(a))
+    rep = str(input("Would you like to make another conversion? (Y or N)"))
+    if rep == "N":
+        repeat = 0
